ca6f2de18a728165bf6710469882a52b401dd982
Ben Word <ben@benword.com>
2016-04-14 13:22:49 -0500

    Latest Trellis

---
+++ trellis/lib/trellis/plugins/vars/vars.py
@@ -0,0 +1,28 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible import __version__
+from ansible.errors import AnsibleError
+
+if __version__.startswith('1'):
+    raise AnsibleError('Trellis no longer supports Ansible 1.x. Please upgrade to Ansible 2.x.')
+
+class VarsModule(object):
+    ''' Creates and modifies host variables '''
+
+    def __init__(self, inventory):
+        self.inventory = inventory
+        self.inventory_basedir = inventory.basedir()
+
+    # Wrap salts and keys variables in {% raw %} to prevent jinja templating errors
+    def wrap_salts_in_raw(self, host, hostvars):
+        if 'vault_wordpress_sites' in hostvars:
+            for name, site in hostvars['vault_wordpress_sites'].iteritems():
+                for key, value in site['env'].iteritems():
+                    if key.endswith(('_key', '_salt')) and not value.startswith(('{% raw', '{%raw')):
+                        hostvars['vault_wordpress_sites'][name]['env'][key] = ''.join(['{% raw %}', value, '{% endraw %}'])
+            host.vars['vault_wordpress_sites'] = hostvars['vault_wordpress_sites']
+
+    def get_host_vars(self, host, vault_password=None):
+        self.wrap_salts_in_raw(host, host.get_group_vars())
+        return {}
